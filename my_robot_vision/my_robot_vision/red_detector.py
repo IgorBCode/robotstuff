298678c04a14bfe092092fecc809172f325cc106
Igor <igorbaranov5@gmail.com>
2025-04-10 17:27:20 -0700

    added red detection

---
+++ my_robot_vision/my_robot_vision/red_detector.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+
+class RedDetector(Node):
+    def __init__(self):
+        super().__init__('red_detector')
+        self.subscription = self.create_subscription(Image, '/camera/image_raw', self.image_callback, 10)
+        self.bridge = CvBridge()
+
+    def image_callback(self, msg):
+        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Red color range in HSV
+        lower_red1 = np.array([0, 100, 100])
+        upper_red1 = np.array([10, 255, 255])
+        lower_red2 = np.array([160, 100, 100])
+        upper_red2 = np.array([179, 255, 255])
+
+        mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+        mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+        mask = cv2.bitwise_or(mask1, mask2)
+
+        # for debugging
+        if cv2.countNonZero(mask) > 0:
+            self.get_logger().info('Red detected')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = RedDetector()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
---
+++ my_robot_vision/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_robot_vision'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='igor',
+    maintainer_email='igorbaranov5@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'red_detector = my_robot_vision.red_detector:main',
+        ],
+    },
+)
